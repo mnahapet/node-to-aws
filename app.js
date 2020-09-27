9d1f9956f2d8d79a75b6b165e47ffdaffa7a38b2
Mariam Nahapetyan <mariam_nahapetyan@yahoo.com>
2020-09-27 19:59:51 +0200

    added env variable

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+require('dotenv').config()
 const app = express();
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the home page baby!')
 })
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000')
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+    console.log('Server running at http://127.0.0.1:' + port + '/');
 });
