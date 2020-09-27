39b3e77ea5bf1fd6b90367d35de1464a55e8e736
Mariam Nahapetyan <mariam_nahapetyan@yahoo.com>
2020-09-27 20:10:19 +0200

    added env variable

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to the home page baby!')
+  res.status(200).send('Welcome to the home page baby the second time!')
 })
 
 const port = process.env.PORT || 3000;
